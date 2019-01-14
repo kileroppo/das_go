7d2063a2d3d3276d6e07ba8b539ccf972566d0e5
nnd1686 <nnd1686@163.com>
2019-01-14 14:45:02 +0800

    修正日志，以及解析OneNET的设备上线通知。 ---jhhe 2019.01.14

--- das/src/httpJob/processJob.go
+++ das/src/httpJob/processJob.go
@@ -1,8 +1,7 @@
 package httpJob
 
 import (
-	"fmt"
-	"encoding/json"
+		"encoding/json"
 	"../core/constant"
 	"../mq/producer"
 	"../core/redis"
@@ -16,11 +15,12 @@ type Serload struct {
 
 type OneMsg struct {
 	At int64			`json:"at"`
-	Msgtype int			`json:"type"`
+	Msgtype int			`json:"type"`				// 数据点消息(type=1)，设备上下线消息(type=2)
 	Value string		`json:"value"`
+	Imei string			`json:"imei"`
 	Dev_id int			`json:"dev_id"`
 	Ds_id string		`json:"ds_id"`
-	Status int			`json:"status"`
+	Status int			`json:"status"`				// 设备上下线标识：0-下线, 1-上线
 	Login_type int		`json:"login_type"`
 }
 type OneNETData struct {
@@ -43,229 +43,224 @@ type Header struct {
 */
 func (p *Serload) ProcessJob() error {
 	// 处理OneNET推送过来的消息
-	log.Debug("process msg from onenet: ", p.pri)
+	log.Info("process msg from onenet: ", p.pri)
 
 	// 1、解析OneNET消息
 	var data OneNETData
-	if err := json.Unmarshal([]byte(p.pri), &data); err == nil {
-		fmt.Println("================json str 转struct==")
-		fmt.Println(data)
-		fmt.Println(data.Msg.Msgtype)
+	if err := json.Unmarshal([]byte(p.pri), &data); err != nil {
+		log.Error("OneNETData json.Unmarshal, err=", err)
 	}
 
-	/*设备上下线消息(type=2)
-	{
-		"msg": {
-		"type": 2,
-			"dev_id": 2016617,
-			"status": 0,
-			"login_type": 1,
-			"at": 1466133706841,
-		},
-		"msg_signature": "message signature",
-		"nonce": abcdefgh"
-	}*/
-	if 1 == data.Msg.Msgtype { // 数据点消息(type=1)，
-		// 2、解析王力的消息
-		//json str 转struct(部份字段)
-		var head Header
-		if err := json.Unmarshal([]byte(data.Msg.Value), &head); err == nil {
-			fmt.Println("================json str 转struct==")
-			fmt.Println(head)
-			fmt.Println(head.Cmd)
-		}
+	switch data.Msg.Msgtype {
+	case 2:	// 设备上下线消息(type=2)
+		{
+			log.Info("OneNET Upload_lock_active")
 
-		// 3、根据命令，分别做业务处理
-		switch head.Cmd {
-		case constant.Add_dev_user: // 添加设备用户
-			{
-				fmt.Printf("constant.Add_dev_user")
-
-				//1. 回复到APP
-				producer.SendMQMsg2APP(head.DevId, p.pri)
-
-				//2. 添加设备用户操作需要存到mongodb
-				if 1 == head.Ack {
-					producer.SendMQMsg2Db(p.pri)
-				}
-			}
-		case constant.Set_dev_user_temp: // 设置临时用户
-			{
-				fmt.Printf("constant.Set_dev_user_temp")
-
-				//1. 回复到APP
-				producer.SendMQMsg2APP(head.DevId, p.pri)
-			}
-		case constant.Add_dev_user_step: // 新增用户步骤
-			{
-				fmt.Printf("constant.Add_dev_user_step")
-			}
-		case constant.Del_dev_user: // 删除设备用户
-			{
-				fmt.Printf("constant.Del_dev_user")
-				//1. 回复到APP
-				producer.SendMQMsg2APP(head.DevId, p.pri)
-			}
-		case constant.Update_dev_user: // 用户更新上报
-			{
-				fmt.Printf("constant.Update_dev_user")
-				//1. 更新设备用户操作需要存到mongodb
-				if 1 == head.Ack {
-					producer.SendMQMsg2Db(p.pri)
-				}
-			}
-		case constant.Sync_dev_user: // 同步设备用户列表
-			{
-				//1. 设备用户同步
-				fmt.Printf("constant.Sync_dev_user")
-				if 1 == head.Ack {
-					producer.SendMQMsg2Db(p.pri)
-				}
-			}
-		case constant.Remote_open: // 远程开锁
-			{
-				fmt.Printf("constant.Remote_open")
-				//1. 回复到APP
-				producer.SendMQMsg2APP(head.DevId, p.pri)
-
-				//2. 远程开门操作需要存到mongodb
-				if 1 == head.Ack {
-					producer.SendMQMsg2Db(p.pri)
-				}
-			}
-		case constant.Upload_dev_info: // 上传设备信息
-			{
-				fmt.Printf("constant.Upload_dev_info")
-				//2. 上传设备信息，需要存到mongodb
-				producer.SendMQMsg2Db(p.pri)
-			}
-		case constant.Volume_level: // 音量等级
-			{
-				fmt.Printf("constant.Volume_level")
-				//1. 回复到APP
-				producer.SendMQMsg2APP(head.DevId, p.pri)
-
-				//2. 需要存到mongodb
-				if 1 == head.Ack {
-					producer.SendMQMsg2Db(p.pri)
-				}
-			}
-		case constant.Open_mode: // 常开模式
-			{
-				fmt.Printf("constant.Open_mode")
-				//1. 回复到APP
-				producer.SendMQMsg2APP(head.DevId, p.pri)
-
-				//2. 需要存到mongodb
-				if 1 == head.Ack {
-					producer.SendMQMsg2Db(p.pri)
-				}
-			}
-		case constant.Passwd_switch: // 密码开关
-			{
-				fmt.Printf("constant.Passwd_switch")
-				//1. 回复到APP
-				producer.SendMQMsg2APP(head.DevId, p.pri)
-
-				//2. 需要存到mongodb
-				if 1 == head.Ack {
-					producer.SendMQMsg2Db(p.pri)
-				}
-			}
-		case constant.Remote_switch: // 远程开关
-			{
-				fmt.Printf("constant.Remote_switch")
-				//1. 回复到APP
-				producer.SendMQMsg2APP(head.DevId, p.pri)
-
-				//2. 需要存到mongodb
-				if 1 == head.Ack {
-					producer.SendMQMsg2Db(p.pri)
-				}
-			}
-		case constant.Sin_mul: // 开门模式
-			{
-				fmt.Printf("constant.Sin_mul")
-				//1. 回复到APP
-				producer.SendMQMsg2APP(head.DevId, p.pri)
-
-				//2. 需要存到mongodb
-				if 1 == head.Ack {
-					producer.SendMQMsg2Db(p.pri)
-				}
-			}
-		case constant.Set_time: // 设置时间
-			{
-				fmt.Printf("constant.Set_time")
-				//1. 回复到APP
-				producer.SendMQMsg2APP(head.DevId, p.pri)
-			}
-		case constant.Update_dev_para: // 设备参数更新上报
-			{
-				fmt.Printf("constant.Update_dev_para")
-				//1. 需要存到mongodb
-				producer.SendMQMsg2Db(p.pri)
-			}
-		case constant.Soft_reset: // 软件复位
-			{
-				fmt.Printf("constant.Soft_reset")
-				//1. 回复到APP
-				producer.SendMQMsg2APP(head.DevId, p.pri)
-			}
-		case constant.Factory_reset: // 恢复出厂设置
-			{
-				fmt.Printf("constant.Factory_reset")
-				//1. 回复到APP
-				producer.SendMQMsg2APP(head.DevId, p.pri)
-			}
-		case constant.Upload_open_log: // 门锁开门日志上报
-			{
-				fmt.Printf("constant.Upload_open_log")
-				//1. 需要存到mongodb
-				producer.SendMQMsg2Db(p.pri)
-			}
-		case constant.Noatmpt_alarm: // 非法操作报警
-			{
-				fmt.Printf("constant.Noatmpt_alarm")
-				//1. 需要存到mongodb
-				producer.SendMQMsg2Db(p.pri)
-			}
-		case constant.Forced_break_alarm: // 强拆报警
-			{
-				fmt.Printf("constant.Noatmpt_alarm")
-				//1. 需要存到mongodb
-				producer.SendMQMsg2Db(p.pri)
-			}
-		case constant.Fakelock_alarm: // 假锁报警
-			{
-				fmt.Printf("constant.Noatmpt_alarm")
-				//1. 需要存到mongodb
-				producer.SendMQMsg2Db(p.pri)
-			}
-		case constant.Nolock_alarm: // 门未关报警
-			{
-				fmt.Printf("constant.Noatmpt_alarm")
-				//1. 需要存到mongodb
-				producer.SendMQMsg2Db(p.pri)
-			}
-		case constant.Low_battery_alarm: // 锁体的电池，低电量报警
-			{
-				fmt.Printf("constant.Noatmpt_alarm")
-				//1. 需要存到mongodb
-				producer.SendMQMsg2Db(p.pri)
-			}
-		case constant.Upload_lock_active: // 锁激活状态上报
-			{
-				fmt.Printf("constant.Noatmpt_alarm")
+			if 1 == data.Msg.Status {
 				//1. 锁唤醒，存入redis
-				// TODO:JHHE
-				redis.SetData(head.DevId, head.Time)
-
-				//2. 回复到APP
-				producer.SendMQMsg2APP(head.DevId, p.pri)
+				redis.SetData(data.Msg.Imei, data.Msg.At/1000)
 			}
-		default:
-			fmt.Printf("Default, Cmd=", head.Cmd)
+		}
+	case 1:	// 数据点消息(type=1)，
+		{
+			// 2、解析王力的消息
+			//json str 转struct(部份字段)
+			var head Header
+			if err := json.Unmarshal([]byte(data.Msg.Value), &head); err != nil {
+				log.Error("Header json.Unmarshal, err=", err)
+			}
+
+			// 3、根据命令，分别做业务处理
+			switch head.Cmd {
+			case constant.Add_dev_user: // 添加设备用户
+				{
+					log.Info("constant.Add_dev_user")
+
+					//1. 回复到APP
+					producer.SendMQMsg2APP(head.DevId, data.Msg.Value)
+
+					//2. 添加设备用户操作需要存到mongodb
+					if 1 == head.Ack {
+						producer.SendMQMsg2Db(data.Msg.Value)
+					}
+				}
+			case constant.Set_dev_user_temp: // 设置临时用户
+				{
+					log.Info("constant.Set_dev_user_temp")
+
+					//1. 回复到APP
+					producer.SendMQMsg2APP(head.DevId, data.Msg.Value)
+				}
+			case constant.Add_dev_user_step: // 新增用户步骤
+				{
+					log.Info("constant.Add_dev_user_step")
+				}
+			case constant.Del_dev_user: // 删除设备用户
+				{
+					log.Info("constant.Del_dev_user")
+					//1. 回复到APP
+					producer.SendMQMsg2APP(head.DevId, data.Msg.Value)
+				}
+			case constant.Update_dev_user: // 用户更新上报
+				{
+					log.Info("constant.Update_dev_user")
+					//1. 更新设备用户操作需要存到mongodb
+					if 1 == head.Ack {
+						producer.SendMQMsg2Db(data.Msg.Value)
+					}
+				}
+			case constant.Sync_dev_user: // 同步设备用户列表
+				{
+					//1. 设备用户同步
+					log.Info("constant.Sync_dev_user")
+					if 1 == head.Ack {
+						producer.SendMQMsg2Db(data.Msg.Value)
+					}
+				}
+			case constant.Remote_open: // 远程开锁
+				{
+					log.Info("constant.Remote_open")
+					//1. 回复到APP
+					producer.SendMQMsg2APP(head.DevId, data.Msg.Value)
+
+					//2. 远程开门操作需要存到mongodb
+					if 1 == head.Ack {
+						producer.SendMQMsg2Db(data.Msg.Value)
+					}
+				}
+			case constant.Upload_dev_info: // 上传设备信息
+				{
+					log.Info("constant.Upload_dev_info")
+					//2. 上传设备信息，需要存到mongodb
+					producer.SendMQMsg2Db(data.Msg.Value)
+				}
+			case constant.Volume_level: // 音量等级
+				{
+					log.Info("constant.Volume_level")
+					//1. 回复到APP
+					producer.SendMQMsg2APP(head.DevId, data.Msg.Value)
+
+					//2. 需要存到mongodb
+					if 1 == head.Ack {
+						producer.SendMQMsg2Db(data.Msg.Value)
+					}
+				}
+			case constant.Open_mode: // 常开模式
+				{
+					log.Info("constant.Open_mode")
+					//1. 回复到APP
+					producer.SendMQMsg2APP(head.DevId, data.Msg.Value)
+
+					//2. 需要存到mongodb
+					if 1 == head.Ack {
+						producer.SendMQMsg2Db(data.Msg.Value)
+					}
+				}
+			case constant.Passwd_switch: // 密码开关
+				{
+					log.Info("constant.Passwd_switch")
+					//1. 回复到APP
+					producer.SendMQMsg2APP(head.DevId, data.Msg.Value)
+
+					//2. 需要存到mongodb
+					if 1 == head.Ack {
+						producer.SendMQMsg2Db(data.Msg.Value)
+					}
+				}
+			case constant.Remote_switch: // 远程开关
+				{
+					log.Info("constant.Remote_switch")
+					//1. 回复到APP
+					producer.SendMQMsg2APP(head.DevId, data.Msg.Value)
+
+					//2. 需要存到mongodb
+					if 1 == head.Ack {
+						producer.SendMQMsg2Db(data.Msg.Value)
+					}
+				}
+			case constant.Sin_mul: // 开门模式
+				{
+					log.Info("constant.Sin_mul")
+					//1. 回复到APP
+					producer.SendMQMsg2APP(head.DevId, data.Msg.Value)
+
+					//2. 需要存到mongodb
+					if 1 == head.Ack {
+						producer.SendMQMsg2Db(data.Msg.Value)
+					}
+				}
+			case constant.Set_time: // 设置时间
+				{
+					log.Info("constant.Set_time")
+					//1. 回复到APP
+					producer.SendMQMsg2APP(head.DevId, data.Msg.Value)
+				}
+			case constant.Update_dev_para: // 设备参数更新上报
+				{
+					log.Info("constant.Update_dev_para")
+					//1. 需要存到mongodb
+					producer.SendMQMsg2Db(data.Msg.Value)
+				}
+			case constant.Soft_reset: // 软件复位
+				{
+					log.Info("constant.Soft_reset")
+					//1. 回复到APP
+					producer.SendMQMsg2APP(head.DevId, data.Msg.Value)
+				}
+			case constant.Factory_reset: // 恢复出厂设置
+				{
+					log.Info("constant.Factory_reset")
+					//1. 回复到APP
+					producer.SendMQMsg2APP(head.DevId, data.Msg.Value)
+				}
+			case constant.Upload_open_log: // 门锁开门日志上报
+				{
+					log.Info("constant.Upload_open_log")
+					//1. 需要存到mongodb
+					producer.SendMQMsg2Db(data.Msg.Value)
+				}
+			case constant.Noatmpt_alarm: // 非法操作报警
+				{
+					log.Info("constant.Noatmpt_alarm")
+					//1. 需要存到mongodb
+					producer.SendMQMsg2Db(data.Msg.Value)
+				}
+			case constant.Forced_break_alarm: // 强拆报警
+				{
+					log.Info("constant.Forced_break_alarm")
+					//1. 需要存到mongodb
+					producer.SendMQMsg2Db(data.Msg.Value)
+				}
+			case constant.Fakelock_alarm: // 假锁报警
+				{
+					log.Info("constant.Fakelock_alarm")
+					//1. 需要存到mongodb
+					producer.SendMQMsg2Db(data.Msg.Value)
+				}
+			case constant.Nolock_alarm: // 门未关报警
+				{
+					log.Info("constant.Nolock_alarm")
+					//1. 需要存到mongodb
+					producer.SendMQMsg2Db(data.Msg.Value)
+				}
+			case constant.Low_battery_alarm: // 锁体的电池，低电量报警
+				{
+					log.Info("constant.Low_battery_alarm")
+					//1. 需要存到mongodb
+					producer.SendMQMsg2Db(data.Msg.Value)
+				}
+			case constant.Upload_lock_active: // 锁激活状态上报
+				{
+					log.Info("constant.Upload_lock_active")
+					//1. 锁唤醒，存入redis
+					redis.SetData(head.DevId, head.Time)
+
+					//2. 回复到APP
+					producer.SendMQMsg2APP(head.DevId, data.Msg.Value)
+				}
+			default:
+				log.Info("Default, Cmd=", head.Cmd)
+			}
 		}
 	}
 	return nil
