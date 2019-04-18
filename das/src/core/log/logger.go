85abcd9333b584be8cab363b3cb7decb60b003b2
nnd1686 <nnd1686@163.com>
2019-04-18 14:25:17 +0800

    日志文件分目录。

--- das/src/core/log/logger.go
+++ das/src/core/log/logger.go
@@ -5,11 +5,13 @@ import (
 	"fmt"
 	"github.com/op/go-logging"
 	"os"
+	"strings"
 	"time"
 	"../file"
 )
 const MaxFileCap = 1024*1024*35
 var m_FileName string
+var m_PathName string
 
 var(
 	ArgsInvaild = errors.New("args can be vaild")
@@ -37,6 +39,7 @@ func NewLogger(pathDir string, level string) {
 	if !flag {
 		os.MkdirAll(destFilePath, os.ModePerm)
 	}
+	m_PathName = destFilePath
 
 	// 文件夹存在, 直接以创建的方式打开文件
 	logFilePath := fmt.Sprintf("%s/%s_%02d%02d%02d%s", destFilePath, "das_go", time.Now().Hour(), time.Now().Minute(), time.Now().Second(),".log")
@@ -71,8 +74,15 @@ func ReNewLogger(pathDir string, level string) {
 	for {
 		time.Sleep(10000) // 10秒检测一次
 
+		//1. 文件大小大于35M，另起文件
 		_, fileSize := file.GetFileByteSize(m_FileName)
-		if int64(fileSize) > int64(MaxFileCap) { // 文件大小大于35M，另起文件
+		if int64(fileSize) > int64(MaxFileCap) {
+			NewLogger(pathDir, level)
+		}
+
+		//2. 每天一个文件夹
+		destFilePath := fmt.Sprintf("%s/%d%02d%02d", pathDir, time.Now().Year(), time.Now().Month(), time.Now().Day())
+		if 0 != strings.Compare(destFilePath, m_PathName) {
 			NewLogger(pathDir, level)
 		}
 	}
