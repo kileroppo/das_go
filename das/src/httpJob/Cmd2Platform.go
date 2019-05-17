ea72f390676fc84c4b2a100e40097f9c57632367
nnd1686 <nnd1686@163.com>
2019-05-17 16:36:34 +0800

    增加命令下发集成文件。

---
+++ das/src/httpJob/Cmd2Platform.go
@@ -0,0 +1,37 @@
+package httpJob
+
+import (
+	"../core/log"
+	"../core/redis"
+	"../core/httpgo"
+)
+
+func Cmd2Platform(imei string, data string) error {
+	platform, errPlat := redis.GetDevicePlatformPool(imei)
+	if errPlat != nil {
+		log.Error("Get Platform from redis failed, err=", errPlat)
+		return errPlat
+	}
+
+	switch platform {
+	case "onenet":
+		{
+			httpgo.Http2OneNET_write(imei, data)
+		}
+	case "telecom":
+		{
+
+		}
+	case "andlink":
+		{
+
+		}
+	default:
+		{
+			log.Error("Unknow Platform from redis, please check the platform: ", platform)
+		}
+	}
+
+	return nil
+
+}
