b0bce72cc23359a6ffcfe848258e07e2ba9638c4
lethe_wzh <eternalzhw@gmail.com>
2019-09-12 11:45:58 +0800

    增加feibee推送数据结构体

--- das/src/feibee2srv/feibee2srv.go
+++ das/src/feibee2srv/feibee2srv.go
@@ -1,1 +1,79 @@
 package feibee2srv
+
+import (
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/dlintw/goconf"
+
+	"../core/log"
+	"../httpJob"
+)
+
+type FeibeeJob struct {
+	rawData []byte
+}
+
+func NewFeibeeJob(rawData []byte) FeibeeJob {
+
+	return FeibeeJob{
+		rawData: rawData,
+	}
+
+}
+
+func (f FeibeeJob) Handle() {
+	log.Debug("onenet job handle")
+	ProcessFeibeeMsg(f.rawData)
+}
+
+func Feibee2HttpSrvStart(conf *goconf.ConfigFile) *http.Server {
+	isHttps, err := conf.GetBool("feibee2http", "is_https")
+
+	if err != nil {
+		log.Errorf("读取https配置失败, %s\n", err)
+		os.Exit(1)
+	}
+
+	httpPort, _ := conf.GetInt("feibee2http", "feibee2http_port")
+
+	srv := &http.Server{
+		Addr: ":" + strconv.Itoa(httpPort),
+	}
+
+	http.HandleFunc("/feibee", FeibeeHandler)
+
+	go func() {
+		if isHttps {
+			serverCrt, _ := conf.GetString("https", "https_server_crt")
+			serverKey, _ := conf.GetString("https", "https_server_key")
+			if err_https := srv.ListenAndServeTLS(serverCrt, serverKey); err_https != nil {
+				log.Error("Httpserver: ListenAndServeTLS(): %s", err_https)
+			}
+		} else {
+			if err_http := srv.ListenAndServe(); err_http != nil {
+				log.Error("feibeeServer ListenAndServer() error=", err_http)
+			}
+		}
+	}()
+
+	return srv
+
+}
+
+func FeibeeHandler(res http.ResponseWriter, req *http.Request) {
+
+	if req.Method != "POST" {
+		log.Error("feibee推送http接口http方法错误")
+	} else {
+		rawData, err := ioutil.ReadAll(req.Body)
+
+		if err != nil {
+			log.Error("get feibee http Body failed")
+		} else {
+			httpJob.JobQueue <- NewFeibeeJob(rawData)
+		}
+	}
+}
--- das/src/feibee2srv/procFeibeeMsg.go
+++ das/src/feibee2srv/procFeibeeMsg.go
@@ -1,6 +1,74 @@
 package feibee2srv
 
-func ProcessFeibeeMsg(DValue string, DevId string) error {
+import (
+	"encoding/json"
+
+	"../core/entity"
+	"../core/log"
+)
+
+type FeibeeData struct {
+	entity.FeibeeData
+}
+
+func NewFeibeeData(data []byte) (FeibeeData, error) {
+	var feibeeData FeibeeData
+
+	if err := json.Unmarshal(data, &feibeeData); err != nil {
+		log.Warning("json.Unmarshal() warning = feibee数据解析失败")
+		return feibeeData, err
+	}
+
+	return feibeeData, nil
+}
+
+func ProcessFeibeeMsg(pushData []byte) (err error) {
+
+	var feibeeData FeibeeData
+	if feibeeData, err = NewFeibeeData(pushData); err != nil {
+		log.Error("NewFeibeeData() error=", err)
+		return
+	}
+
+	//feibee数据合法性检查
+	if !feibeeData.isDataValid() {
+		log.Warning("the feibee message'type is invalid")
+		return nil
+	}
+
+	//feibee数据格式化为本地协议
+	if err := feibeeData.dataFormat(); err != nil {
+		return err
+	}
+
+	//feibee数据推送到mq
+	if err := feibeeData.push2mq(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (f FeibeeData) isDataValid() bool {
+	if f.Status != "" && f.Ver != "" {
+		if f.Msg != nil || f.Gateway != nil {
+			switch f.Code {
+			case 3, 4, 5, 12, 15, 32:
+				return true
+			default:
+				return false
+			}
+		}
+	}
+	return false
+}
+
+func (f FeibeeData) dataFormat() error {
+
+	return nil
+}
+
+func (f FeibeeData) push2mq() error {
 
 	return nil
 }
