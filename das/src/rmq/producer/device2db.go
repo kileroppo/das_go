86121e29f833176d5c4a2f8e2a9667ac9f3e87e1
nnd1686 <nnd1686@163.com>
2019-09-18 10:30:47 +0800

    修改rmq2db，分开为两个交换器，两个队列。

--- das/src/rmq/producer/device2db.go
+++ das/src/rmq/producer/device2db.go
@@ -8,6 +8,7 @@ import (
 
 var rmq_uri_mgo string
 var exchange_mgo string         // = "OneNET2APP"
+var exchange_mgo2 string
 var exchangeType_mgo string     // = "direct"
 var routingKey_mgo string = ""  // 设备的uuid
 var routingKey_mgo2 string = "" // 设备的uuid
@@ -22,6 +23,8 @@ func InitRmq_Ex_Que_Name_mongo(conf *goconf.ConfigFile) {
 	exchange_mgo, _ = conf.GetString("rabbitmq", "Device2Db_ex")
 	exchangeType_mgo, _ = conf.GetString("rabbitmq", "device2db_ex_type")
 	routingKey_mgo, _ = conf.GetString("rabbitmq", "device2db_que")
+
+	exchange_mgo2, _ = conf.GetString("rabbitmq", "Device2Db_ex2")
 	routingKey_mgo2, _ = conf.GetString("rabbitmq", "device2db_que2")
 }
 
@@ -43,8 +46,8 @@ func SendMQMsg2Db2(message string) {
 		return
 	}
 
-	channleContxt := rabbitmq.ChannelContext{Exchange: exchange_mgo, ExchangeType: exchangeType_mgo, RoutingKey: routingKey_mgo2, Reliable: true, Durable: true, ReSendNum: 0}
+	channleContxt := rabbitmq.ChannelContext{Exchange: exchange_mgo2, ExchangeType: exchangeType_mgo, RoutingKey: routingKey_mgo2, Reliable: true, Durable: true, ReSendNum: 0}
 
 	log.Info("rabbitmq.ProducerRabbitMq.Publish2Db: ", message)
-	rabbitmq.ProducerRabbitMq2Db.Publish2Db2(&channleContxt, message)
+	rabbitmq.ProducerRabbitMq2Db.Publish2Db(&channleContxt, message)
 }
