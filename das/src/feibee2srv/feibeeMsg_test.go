ce5bd2b6de81df9fb4ca4adc5a057f57a3a9c179
lethe_wzh <eternalzhw@gmail.com>
2020-02-11 14:49:56 +0800

    type:test scope: subject:新增飞比场景消息单元测试

--- das/src/feibee2srv/feibeeMsg_test.go
+++ das/src/feibee2srv/feibeeMsg_test.go
@@ -2,7 +2,8 @@ package feibee2srv
 
 import "testing"
 
-var pushData = `
+var (
+	devAdd = `
 {
     "code": 3,
     "status": "newdevice",
@@ -27,13 +28,110 @@ var pushData = `
     ]
 }
 `
+	sceneAdd = `
+{
+  "code": 21,
+  "status": "addscene",
+  "ver": "2",
+  "sceneMessages": [
+    {
+      "scenes": [
+        {
+          "sceneName": "abcdd2",
+          "sceneID": 2,
+          "sceneMembers": [
+            {
+              "deviceuid": 737925,
+              "deviceID": 528,
+              "data1": 1,
+              "data2": 45,
+              "data3": 45,
+              "data4": 45,
+              "IRID": 0,
+              "delaytime": 0,
+              "sceneFunctionID": 0,
+              "uuid": "00124b0014afcbbf_0b"
+            }
+          ]
+        }
+      ],
+      "bindid": "2201129"
+    }
+  ]
+}
+`
+	sceneRename = `
+{
+  "code": 23,
+  "status": "scenename",
+  "ver": "2",
+  "sceneMessages": [
+    {
+      "scenes": [
+        {
+          "sceneName": "abcd34",
+          "sceneID": 1
+        }
+      ]
+    }
+  ]
+}
+`
+
+	sceneDel = `
+{
+  "code": 22,
+  "status": "removescene",
+  "ver": "2",
+  "sceneMessages": [
+    {
+      "scenes": [
+        {
+          "sceneName": "abcdd2",
+          "sceneID": 1,
+          "sceneMembers": [
+            {
+              "deviceuid": 1236014,
+              "deviceID": 2,
+              "data1": 0,
+              "data2": 0,
+              "data3": 0,
+              "data4": 0,
+              "IRID": 0,
+              "delaytime": 0,
+              "sceneFunctionID": 0,
+              "uuid": "00124b00092a5de7_12"
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
+`
+)
 
 func TestProcessFeibeeMsg(t *testing.T) {
-	if ProcessFeibeeMsg([]byte(pushData)) != nil {
-		t.Error("数据解析错误")
+	var tests = []struct {
+		msgName string
+		msgValue string
+	}{
+		{"devAdd", devAdd},
+		{"sceneAdd", sceneAdd},
+		{"sceneDel", sceneDel},
+		{"sceneRename", sceneRename},
+	}
+
+	for _,ts := range tests {
+		if ProcessFeibeeMsg([]byte(ts.msgValue)) != nil {
+			t.Errorf("Process %s error", ts.msgName)
+		}
 	}
 }
 
 func BenchmarkFeibeeProc(b *testing.B) {
-	ProcessFeibeeMsg([]byte(pushData))
+	for i:=0;i<b.N;i++ {
+		ProcessFeibeeMsg([]byte(sceneAdd))
+	}
 }
+
