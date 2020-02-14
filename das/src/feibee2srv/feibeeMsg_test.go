a03d6b6f8b701988cadf45f91d83c688b646dcdb
lethe_wzh <eternalzhw@gmail.com>
2020-02-14 15:25:03 +0800

    type:test scope: subject:增加小卫士数据解析单元测试用例

--- das/src/feibee2srv/feibeeMsg_test.go
+++ das/src/feibee2srv/feibeeMsg_test.go
@@ -1,6 +1,10 @@
 package feibee2srv
 
-import "testing"
+import (
+	"das/core/log"
+	"das/core/rabbitmq"
+	"testing"
+)
 
 var (
 	devAdd = `
@@ -109,7 +113,15 @@ var (
   ]
 }
 `
+	lGuardScene = `
+{"code":2,"status":"report","ver":"2","records":[{"bindid":"5233586","deviceuid":85390,"cid":0,"aid":16652,"value":"06ab01230141e7","orgdata":"700f8e4d010000010c414205ab000b41e7","pushstring":"","uuid":"00158d0003e8b2e3_01","snid":"FNB54-HWD01WL0.7","devicetype":"0x30b0001","deviceid":779,"zonetype":1,"uptime":1581527097545}]}
+`
 )
+
+func init() {
+	conf := log.Init()
+	rabbitmq.Init(conf)
+}
 
 func TestProcessFeibeeMsg(t *testing.T) {
 	var tests = []struct {
@@ -127,6 +139,10 @@ func TestProcessFeibeeMsg(t *testing.T) {
 			t.Errorf("Process %s error", ts.msgName)
 		}
 	}
+}
+
+func TestPrcessOneMsg(t *testing.T) {
+	ProcessFeibeeMsg([]byte(lGuardScene))
 }
 
 func BenchmarkFeibeeProc(b *testing.B) {
