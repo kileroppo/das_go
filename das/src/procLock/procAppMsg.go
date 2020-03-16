d1472c3385b2d0d478697cd5229e11d939dd0e22
nnd1686 <nnd1686@163.com>
2020-03-16 09:50:10 +0800

    修改平板锁视频请求。

--- das/src/procLock/procAppMsg.go
+++ das/src/procLock/procAppMsg.go
@@ -258,7 +258,7 @@ func ProcAppMsg(appMsg string) error {
 		{
 			var strToDevData string
 			var err error
-			if "WL025S1" == head.DevType && 0x36 == head.Cmd { // TODO:JHHE 临时方案，平板锁开启视频不加密
+			if 0x1001 == head.Cmd { // TODO:JHHE 临时方案，平板锁开启视频不加密
 				strToDevData = appMsg
 			} else {
 				// 加密数据
