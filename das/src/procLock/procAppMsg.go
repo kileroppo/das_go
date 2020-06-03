07e4ade8b60fd94af0a94aa57132af1ad5a72c6b
nnd1686 <nnd1686@163.com>
2020-06-03 17:45:40 +0800

    增加错误判断。

--- das/src/procLock/procAppMsg.go
+++ das/src/procLock/procAppMsg.go
@@ -29,6 +29,10 @@ func ProcAppMsg(appMsg string) error {
 		/*log.Error("ProcAppMsg() error msg : ", appMsg)
 		return errors.New("error msg.")*/
 		// TODO:JHHE APP下行数据解密
+		if !strings.ContainsAny(appMsg, "#") {
+			log.Error("ProcAppMsg() error msg")
+			return errors.New("error msg.")
+		}
 		//1. 获取设备编号
 		prData := strings.Split(appMsg, "#")
 		var devID string
