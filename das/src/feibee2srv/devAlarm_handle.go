dcc925a8084b3113e7ea004ebd215a46b3abb251
lethe_wzh <eternalzhw@gmail.com>
2020-07-23 14:16:08 +0800

    type:fix scope: subject:调整设备电量上报json字段

--- das/src/feibee2srv/devAlarm_handle.go
+++ das/src/feibee2srv/devAlarm_handle.go
@@ -75,7 +75,7 @@ func (self *BaseSensorAlarm) PushMsg() {
 	if self.alarmType == "doorContact" {
 		self.pushMsg2mns()
 	} else if self.alarmType == "lowPower" {
-		self.pushAlarmMsg2app()
+		self.pushStatusMsg2app("power")
 		if (self.alarmFlag < 30) {
 			self.pushMsg2mns()
 		}
@@ -91,8 +91,8 @@ func (self *BaseSensorAlarm) PushMsg() {
 	self.pushForcedBreakMsg()
 }
 
-func (self *BaseSensorAlarm) pushStatusMsg2app() {
-	msg := self.createStatusMsg()
+func (self *BaseSensorAlarm) pushStatusMsg2app(opType string) {
+	msg := self.createStatusMsg(opType)
 
 	data, err := json.Marshal(msg)
 	if err != nil {
@@ -144,7 +144,7 @@ func (self *BaseSensorAlarm) createMsg2pmsForSence() entity.Feibee2AutoSceneMsg 
 	return msg
 }
 
-func (self *BaseSensorAlarm) createStatusMsg() entity.Feibee2DevMsg {
+func (self *BaseSensorAlarm) createStatusMsg(opType string) entity.Feibee2DevMsg {
 	var msg entity.Feibee2DevMsg
 
 	msg.Cmd = 0xfb
@@ -154,7 +154,7 @@ func (self *BaseSensorAlarm) createStatusMsg() entity.Feibee2DevMsg {
 	msg.Vendor = "feibee"
 	msg.SeqId = 1
 
-	msg.OpType = self.alarmType
+	msg.OpType = opType
 	msg.OpValue = strconv.Itoa(self.alarmFlag)
 	msg.Time = self.time
 
@@ -233,7 +233,7 @@ func (c *ContinuousSensor) PushMsg() {
 	//todo: 其他类型暂不推送mns
 	//c.pushMsg2mns()
 	if c.msgType == FloorHeat || c.msgType == Airer {
-		c.pushStatusMsg2app()
+		c.pushStatusMsg2app(c.alarmType)
 	}
 
 	if c.msgType == TemperAndHumiditySensor || c.msgType == IlluminanceSensor || c.msgType == Airer {
