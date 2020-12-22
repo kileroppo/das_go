8d85af1cea7097fee743466fcbd4746d531300df
lethe_wzh <eternalzhw@gmail.com>
2020-12-22 13:37:23 +0800

    type:feat scope: subject:享睡睡眠带状态变化频率控制

--- das/src/tuya2srv/filter.go
+++ das/src/tuya2srv/filter.go
@@ -4,7 +4,7 @@ import "das/filter"
 
 func tyAlarmMsgFilter(devId, code string, val interface{}) bool {
 	if _,ok := tyAlarmDataFilterMap[code]; ok {
-		res := filter.AlarmMsgFilter(devId + code, val)
+		res := filter.AlarmMsgFilter(devId + code, val, -1)
 		return res
 	} else {
 		return true
